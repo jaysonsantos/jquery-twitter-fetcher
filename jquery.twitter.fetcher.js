30bad67c7c42a78ca34e6a189e887792672203da
Jayson Reis <jayson.reis@sabbre.com.br>
2011-07-04 10:52:19 -0300

    removed strange format

--- jquery.twitter.fetcher.js
+++ jquery.twitter.fetcher.js
@@ -64,12 +64,12 @@
     }
     
     $.fn.twitter_fetcher = function (method) {
-        if ( methods[method] ) {
-            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-        } else if ( typeof method === 'object' || ! method ) {
-            return methods.init.apply( this, arguments );
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || ! method) {
+            return methods.init.apply(this, arguments);
         } else {
-            $.error( 'Method ' +  method + ' does not exist on jQuery.twitter.fetcher' );
+            $.error('Method ' +  method + ' does not exist on jQuery.twitter.fetcher');
         }
     };
 
